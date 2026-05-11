d42a443e28aca4a50ed0857bd4d2dbc206622ab1
Mihaash <mihaashdharan68@gmail.com>
2026-05-11 13:43:31 +0530

    testing

--- src/test/java/com/myapp/MyappServerTests.java
+++ src/test/java/com/myapp/MyappServerTests.java
@@ -1,15 +1,22 @@
 package com.myapp;
 
+import com.myapp.controller.CarController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = MyappServer.class)
 class MyappServerTests
 {
+    @Autowired
+    private CarController carController;
 
     @Test
     void contextLoads()
     {
+        assertNotNull(carController);
     }
 
 }
